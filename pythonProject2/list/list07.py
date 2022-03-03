6ea04759d044262d9bd45abbe0f89f53b6a72044
lim jungwon <jungwonalicia@gmail.com>
2022-03-03 17:48:14 +0900

    python list4

--- pythonProject2/list/list07.py
+++ pythonProject2/list/list07.py
@@ -15,10 +15,21 @@ while True:
     print('\n-----------------------------')
     no = int(input('예매 좌석번호 선택(종료 -1)>> '))
     if no == -1:
-        break
         # 몇 좌석 예매가 되었는지 프린트
+        # => list에 1이 몇 개가 있는지 카운트
+        seat_count = seat.count(1)
+        print('좌석 개수>>', seat_count)
+
         # 결제금액 한 자리당 1만원해서 프린트
+        price = 10000
+        print('예매 금액>> %d원' % (price * seat_count))
         # 좌석 번호 프린트
+        seat_list = []
+        for i in range(len(seat)):
+            if(seat[i] == 1):
+                seat_list.append(i)
+        print('예매한 좌석은>> %s ' % seat_list)
+        break
     else:
         # 예매처리
         seat[no] = 1
