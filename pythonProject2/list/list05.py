03391db332c46b4a250fc025a2e21f901c820f17
lim jungwon <jungwonalicia@gmail.com>
2022-03-03 15:37:17 +0900

    python list

---
+++ pythonProject2/list/list05.py
@@ -0,0 +1,21 @@
+start, end = input('시작, 끝값 입력>>').split(',')
+start2 = int(start)
+end2 = int(end)
+print(start2)
+print(end2)
+
+length = len(list(range(start2, end2 + 1)))
+print('개수는 ', length)
+
+sum = 0
+for x in range(start2, end2 + 1):
+    sum += x
+print('sum: %d점' % sum )
+print('avg: %0.1f' % (sum / length))
+
+print('--------')
+
+sum = 0
+for n in range(10, 21):
+    sum += n
+print(sum)
---
+++ pythonProject2/list/list06.py
@@ -0,0 +1,15 @@
+target = 55
+
+cnt = 0
+while True:
+    number = int(input('숫자를 입력>>'))
+    cnt += 1
+    if number != target:
+        if(number > target):
+            print('too big')
+        else:
+            print('too small')
+    else:
+        print('correct!')
+        print('try cnt>> ', cnt)
+        break
