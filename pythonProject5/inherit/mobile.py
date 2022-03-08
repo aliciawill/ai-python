f9fd3a38fbb7325ec4498a86ac4fc4b6b27e34c0
lim jungwon <jungwonalicia@gmail.com>
2022-03-08 11:13:03 +0900

    inherit

---
+++ pythonProject5/inherit/mobile.py
@@ -0,0 +1,26 @@
+class Phone:
+    def text(self):
+        print('문자를 보내다.')
+
+    def call(self):
+        print('전화통화하다.')
+
+
+class SamsungPhone(Phone):
+    name = ''
+
+    def game(self):
+        print(self.name + '과 게임하다.')
+
+    def internet(self, time):  # 지역함수 internet(3)
+        print(str(time) + '시간 인터넷하다.')
+
+
+class ApplePhone(Phone):
+    size = 0
+
+    def picture(self):
+        print('사진을 찍다.')
+
+    def youtube(self, time, subject):
+        print(str(time) + '시간동안 ' + subject + '라는 주제로 유튜브하다.')
