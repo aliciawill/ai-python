f34a44ce8155a8c3414437d91f6a367d995436be
aliciawill <aliciawill@kakao.com>
2022-05-24 11:45:26 +0900

    member

--- djangoProject3/member/views.py
+++ djangoProject3/member/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from member.models import Question, Test
 
 # Create your views here.
 def start(req):
@@ -15,4 +16,16 @@ def insert(req):
     return render(req, 'member/insert.html')
 
 def insert2(req):
-    return HttpResponse('회원 가입 처리 페이지')
+    #1. 입력한 정보 받아서
+    data = req.POST #dic
+    print('서버에서 받은 데이터 >> ', data)
+    print('name: ', data['name'])
+    print('tel: ', data['tel'])
+    print('addr: ', data['addr'])
+    #2. db처리하고
+    ##2-1.객체 생성
+    one = Test(name=data['name'], tel=data['tel'], addr=data['addr'])
+    ##2-2.save()
+    one.save()
+    #3. 결과를 알려주자.
+    return render(req, 'member/insert2.html')
